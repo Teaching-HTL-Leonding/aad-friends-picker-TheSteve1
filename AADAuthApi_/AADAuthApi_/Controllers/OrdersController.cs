3672c765236e028edf7b8265a3ac5acb22fd5448
TheSteve1 <68355734+TheSteve1@users.noreply.github.com>
2021-03-11 17:08:39 +0100

    first Commit

---
+++ AADAuthApi_/AADAuthApi_/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AADAuthApi_.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class OrdersController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetAllOrders()
+		{
+			return Ok(new object[] { "Order 1", "Order 2" });
+		}
+	}
+}
